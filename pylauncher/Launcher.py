c30f0993306b32c8948c29488b7442290f6311c2
phizzl3 <phizzl3@gmail.com>
2021-01-17 19:47:28 -0600

    Update Launcher.py -Adds sends commands based on platform and env -Adds exit command to break out of loop -Adds main function

--- pylauncher/Launcher.py
+++ pylauncher/Launcher.py
@@ -3,41 +3,67 @@
 
 from pathlib import Path
 from menuloop import display_menu
-
+import subprocess
 import os
-
+import platform
 
 # Get code path
 CODE = Path(__file__).resolve().parent.parent.parent
-ENV = 'env/Scripts/activate'
 
+
+OS = platform.system()
 
 # Set up dict or class
+
+
 class CMD:
 
-    def __init__(self, scrptpath, envpath=False) -> None:
+    def __init__(self, repopath, runptpath, env=False) -> None:
 
-        self.command = f'py {CODE}{scrptpath}'
-        if envpath:
-            self.environment = envpath
+        self.repopath = f'{CODE}{repopath}'
+        self.runpath = f'{CODE}{runptpath}'
+        self.env = env
 
     def send_commands(self) -> None:
         # write this to call the windows commands
         # cli > start env (if True) && launch .py
-        os.system(f'{CODE}{self.environment}{ENV} && {self.command}')
-        pass
+
+        if self.env == exit:
+            exit('Exiting...')
 
 
-# Generate objects and list
-def go():
+        # Windows
+        if OS == 'Windows':
+            if self.env:
+                os.system(
+                    f'{self.repopath}/env/Scripts/activate && py {self.runpath}')
+            else:
+                os.system(f'py {self.runpath}')
 
-    # Display menu
-    display_menu(
-        ('Create Papercut Packages', CMD(
-            '/papercut-sw-packager/pcswpkgr/papercut_sw_packager.py', 
-            '/papercut-sw-packager/'))
-    ).send_commands()  # Send terminal command
+        # Mac
+        elif OS == 'Darwin':
+            if self.env:
+                subprocess.run(
+                    f'source {self.repopath}/env/bin/activate && python3 {self.runpath}', shell=True)
+            else:
+                subprocess.run(f'python3 {self.runpath}', shell=True)
 
+        else:
+            exit('Platform not supported.')
+
+
+def main():
+
+    while True:
+        # Display menu
+        display_menu(
+            ('Create Papercut Packages', CMD(
+                '/papercut-sw-packager',
+                '/papercut-sw-packager/pcswpkgr/papercut_sw_packager.py',
+                env=True
+            )),
+            ('Exit', CMD(None, None, exit))
+        ).send_commands()  # Send terminal command
 
 if __name__ == "__main__":
-    go()
+    main()
