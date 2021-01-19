7730b5dc6e0dc7cd96de6d54ba3fdd4a8e45201a
Brandon <phizzl3@gmail.com>
2021-01-19 16:28:43 -0600

    Adds exception handling

--- pylauncher/PyLauncher.py
+++ pylauncher/PyLauncher.py
@@ -34,33 +34,43 @@ class CMD:
             envfolder (str, optional): Folder name within the repofolder 
                 for your virtualenv folder if applicable. Defaults to None.
         """
-        # Sets attributes containing Path objects pointing to specified folders and file
-        self.repofolder = CODE / repofolder
-        self.runpath = self.repofolder / runptpath
+        try:
+            # Sets attributes containing Path objects pointing to specified folders and file
+            self.repofolder = CODE / repofolder
+            self.runpath = self.repofolder / runptpath
+            
+            if envfolder:
+                self.envfolder = self.repofolder / envfolder
 
-        if envfolder:
-            self.envfolder = self.repofolder / envfolder
+        except Exception as e:
+            input(f'\n Error running __init__: {e}')
+            exit(' Exiting...')
 
     def send_commands(self) -> None:
         """
         Generates and runs shell commands based on detected operating system and 
         specified virtualenv if present. (Windows, Mac, Linux)
         """
-        # Set up an exit option to break out of the loop
-        if str(self.repofolder).endswith('EXIT'):
-            exit('Exiting...')
+        try:
+            # Set up an exit option to break out of the loop
+            if str(self.repofolder).endswith('EXIT'):
+                exit(' Exiting...')
 
-        # Run correctly formatted command based on detected OS and virtual environment
-        if OS == 'Windows':
-            os.system(
-                f'{self.envfolder/"Scripts"/"python.exe" if hasattr(self, "envfolder") else "py"} {self.runpath}')
+            # Run correctly formatted command based on detected OS and virtual environment
+            if OS == 'Windows':
+                os.system(
+                    f'{self.envfolder/"Scripts"/"python.exe" if hasattr(self, "envfolder") else "py"} {self.runpath}')
 
-        elif OS == 'Darwin' or 'Linux':
-            subprocess.run(
-                f'{self.envfolder/"bin"/"python3" if hasattr(self, "envfolder") else "python3"} {self.runpath}', shell=True)
+            elif OS == 'Darwin' or 'Linux':
+                subprocess.run(
+                    f'{self.envfolder/"bin"/"python3" if hasattr(self, "envfolder") else "python3"} {self.runpath}', shell=True)
 
-        else:
-            exit('Platform not supported.')
+            else:
+                exit('Platform not supported.')
+
+        except Exception as e:
+            input(f'\n Error running send_commands: {e}')
+            exit(' Exiting...')
 
 
 def main():
@@ -68,17 +78,20 @@ def main():
     Generates objects, displays the selection menu and calls the 
     send_commands method on returned object based on user selction.
     """
-    while True:
-        display_title()
-        # Generate objects and send to display menu (*EXAMPLES*)
-        display_menu(
-            ('_Menu option 1', CMD('_repo folder 1', '_src/main.py', envfolder='_env')),
-            ('_Menu option 2', CMD('_repo folder 2',
-                                   '_src2/main.py', envfolder='_venv')),
-            ('_Menu option 3', CMD('_repo folder 3', '_src3/main.py', envfolder=None)),
-            # Leave this one last for an exit option
-            ('Exit', CMD('EXIT', ''))
-        ).send_commands()  # Call send_commands method on object returned from menu selection
+    try:
+        while True:
+            display_title()
+            # Generate objects and send to display menu (*EXAMPLES*)
+            display_menu(('_Menu option 1', CMD('_repo folder 1', '_src/main.py', envfolder='_env')),
+                ('_Menu option 2', CMD('_repo folder 2', '_src2/main.py', envfolder='_venv')),
+                ('_Menu option 3', CMD('_repo folder 3', '_src3/main.py', envfolder=None)),
+                # Leave this one last for an exit option
+                ('Exit', CMD('EXIT', ''))
+            ).send_commands()  # Call send_commands method on object returned from menu selection
+
+    except Exception as e:
+        input(f'\n Error running main: {e}')
+        exit(' Exiting...')
 
 
 if __name__ == "__main__":
